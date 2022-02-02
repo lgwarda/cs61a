4570ede55bc85fb09e2b0cf88faa0f6812ec7942
Lukasz Gwarda <kukikarma@gmail.com>
2022-02-02 15:58:44 +0100

    Implement play function.

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -166,6 +166,18 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    while score0 < goal and score1 < goal:
+        if who == 0:
+            score_gained = take_turn(strategy0(score0, score1), score1, dice)
+            score0 +=  score_gained
+            if not extra_turn(score0, score1):
+                who = other(who)
+        else:
+            score_gained = take_turn(strategy1(score1, score0), score0, dice)
+            score1 += score_gained
+            if not extra_turn(score1, score0):
+                who = other(who)
+            
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
