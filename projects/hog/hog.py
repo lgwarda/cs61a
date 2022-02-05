b7974aa77c29d4107681905d304833e0765e3652
Lukasz Gwarda <kukikarma@gmail.com>
2022-02-05 14:47:19 +0100

    Implement the extra_turn_strategy function.

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -398,7 +398,10 @@ def extra_turn_strategy(score, opponent_score, cutoff=8, num_rolls=6):
     Otherwise, it rolls NUM_ROLLS.
     """
     # BEGIN PROBLEM 11
-    return 6  # Replace this statement
+    if extra_turn(free_bacon(opponent_score) + score, opponent_score):
+        return 0
+    else:
+        return bacon_strategy(score, opponent_score, cutoff, num_rolls) 
     # END PROBLEM 11
 
 
