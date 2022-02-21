f19cbeb3f41bc689fe85be109fcd047791ada66a
Lukasz Gwarda <kukikarma@gmail.com>
2022-02-21 19:53:58 +0100

    Implement add_chars function.

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -151,4 +151,9 @@ def add_chars(w1, w2):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if w1 == "":
+        return w2
+    elif w1[0] != w2[0]:
+        return w2[0] + add_chars(w1[:], w2[1:])
+    else:
+        return  add_chars(w1[1:], w2[1:])
