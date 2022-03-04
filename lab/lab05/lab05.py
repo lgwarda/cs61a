ad04609e09281c928b60288ea6ca0fed16d50858
Lukasz Gwarda <kukikarma@gmail.com>
2022-03-04 08:49:41 +0100

    Implement coords function.

--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -257,7 +257,7 @@ def coords(fn, seq, lower, upper):
     [[-2, 4], [1, 1], [3, 9]]
     """
     "*** YOUR CODE HERE ***"
-    return ______
+    return [[x, fn(x)] for x in seq if fn(x) <= upper and fn(x) >= lower]
 
 
 def riffle(deck):
