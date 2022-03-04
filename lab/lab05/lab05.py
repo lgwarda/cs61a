d119b884907278551f9208d27b42356d918ff713
Lukasz Gwarda <kukikarma@gmail.com>
2022-03-04 07:48:22 +0100

    Implement berry_finder function.

--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -153,7 +153,13 @@ def berry_finder(t):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if label(t) == 'berry':
+        return True
+    for node in branches(t):
+        if berry_finder(node):
+            return True
+    else:
+        return  False
 
 def sprout_leaves(t, leaves):
     """Sprout new leaves containing the data in leaves at each leaf in
